0d5a695a618a8ca0b5199d44146f106ef171f7de
PrizZzt <npuzZzpak@yandex.ru>
2019-02-27 20:27:25 +0400

    Добавлен клиент для .Net Core с проектом Visual Studio

---
+++ CodeBattle.netcore-vs/CodeBattleNetCore/Program.cs
@@ -0,0 +1,42 @@
+﻿using CodeBattleNetCoreLibrary;
+using System;
+using Microsoft.Extensions.Configuration;
+
+namespace CodeBattleNetCore
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var settings = new ConfigurationBuilder()
+					.AddJsonFile("appsettings.json", false)
+					.AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", true)
+					.Build().Get<Settings>();
+
+			var gcb = new GameClientStarMarines(settings.Server, settings.BotName, settings.Token);
+			gcb.Run(() =>
+			{
+				if (gcb.GetErrors().Count != 0)
+				{
+					foreach (var error in gcb.GetErrors())
+					{
+						Console.WriteLine(error);
+					}
+				}
+				foreach (var planet in gcb.GetMyPlanets())
+				{
+					foreach (var neighbour in planet.Neighbours)
+					{
+						if (neighbour != planet.Id)
+						{
+							gcb.SendDrones(planet.Id, neighbour, planet.Droids / planet.Neighbours.Length);
+						}
+					}
+				}
+				gcb.SendMessage();
+			});
+
+			Console.ReadKey(true);
+		}
+	}
+}
---
+++ CodeBattle.netcore-vs/CodeBattleNetCore/Settings.cs
@@ -0,0 +1,11 @@
+﻿namespace CodeBattleNetCore
+{
+	public class Settings
+	{
+		public string BotName { get; set; }
+
+		public string Server { get; set; }
+
+		public string Token { get; set; }
+	}
+}
